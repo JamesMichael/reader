c33890f6db418e9d851d4cd6733f91ec8dfe5281
James Michael <jamesamichael@gmail.com>
2013-06-17 20:59:37 +0100

    Order feed fetches relation by fetch date

--- model/lib/Reader/Model/Result/Feed.pm
+++ model/lib/Reader/Model/Result/Feed.pm
@@ -18,5 +18,5 @@ __PACKAGE__->table('feed');
 __PACKAGE__->add_columns(qw( id uri title link description author priority ));
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->has_many(items => 'Reader::Model::Result::Item', 'feed_id');
-__PACKAGE__->has_many(fetches => 'Reader::Model::Result::Fetch', 'feed_id');
+__PACKAGE__->has_many(fetches => 'Reader::Model::Result::Fetch', 'feed_id', { order_by => { -desc => 'fetch_date' } });
 1;
