a54952e2372e1e59c6c7facfb7eb1ddf073158ec
James Michael <jamesamichael@gmail.com>
2013-06-24 20:45:10 +0100

    Add message column to fetch table

--- model/db/fetch.sql
+++ model/db/fetch.sql
@@ -3,6 +3,7 @@ CREATE TABLE fetch (
     feed_id INTEGER NOT NULL REFERENCES feed(id),
     fetch_date INTEGER NOT NULL,
     filename TEXT,
-    status TEXT NOT NULL
+    status TEXT NOT NULL,
+    message TEXT NOT NULL DEFAULT ''
 );
 
--- model/lib/Reader/Model/Result/Fetch.pm
+++ model/lib/Reader/Model/Result/Fetch.pm
@@ -2,7 +2,7 @@ package Reader::Model::Result::Fetch;
 use base qw(DBIx::Class::Core);
 
 __PACKAGE__->table('fetch');
-__PACKAGE__->add_columns(qw( id feed_id fetch_date filename status ));
+__PACKAGE__->add_columns(qw( id feed_id fetch_date filename status message ));
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->belongs_to(feed => 'Reader::Model::Result::Feed', 'feed_id');
 1;
