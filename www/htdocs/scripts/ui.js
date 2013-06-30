b885109207a984f8304087a7d5dd6e973cb31adf
James Michael <jamesamichael@gmail.com>
2013-06-30 15:22:11 +0100

    Add code to select items

--- www/htdocs/scripts/ui.js
+++ www/htdocs/scripts/ui.js
@@ -1,5 +1,6 @@
 var UI = (function($) {
     var ui = {};
+    var selected_item_index = 0;
 
 	function format_item(item) {
         var content = [
@@ -13,8 +14,19 @@ var UI = (function($) {
 		return content;
 	}
 
+	function select_item(index) {
+	    var container = $('#item_container');
+
+	    // de-select current item
+	    container.find('item-opened').removeClass('item-opened').addClass('item-closed');
+
+	    // select new item
+        container.children().eq(index).removeClass('item-closed').addClass('item-opened');
+    }
+
     ui.load_unread_items = function() {
         var container = $('#item_container');
+        container.empty();
 
 		$.when(API.get_next_unread_items()).then(function(data) {
 			data.items.forEach(function(item) {
@@ -22,6 +34,9 @@ var UI = (function($) {
 				container.append(content);
 			});
 		});
+
+		selected_item_index = 0;
+		select_item(selected_item_index);
 	};
 
     return ui;
