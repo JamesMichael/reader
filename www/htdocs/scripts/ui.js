ec120225538c1c4078dab3ca3c20d5bd83606f7e
James Michael <jamesamichael@gmail.com>
2013-07-01 18:53:09 +0100

    Mark items as read when viewed (unless they have been manaully kept unread)

--- www/htdocs/scripts/ui.js
+++ www/htdocs/scripts/ui.js
@@ -65,14 +65,21 @@ var UI = (function($) {
 	    container.find('.item-opened').removeClass('item-opened').addClass('item-closed');
 
 	    // select new item
-        container.children().eq(index).removeClass('item-closed').addClass('item-opened');
+	    var item = container.children().eq(index);
+	    item.removeClass('item-closed').addClass('item-opened');
 
         selected_item_index = index;
 
         scroll_to_item(index);
 
-        var item_id = element_id_to_item_id(container.children().eq(index).attr('id'));
+        var item_id = element_id_to_item_id(item.attr('id'));
         build_action_panel(item_id);
+
+        // mark item as read
+        // unless the user has previously kept the item unread
+        if (item.data('kept-unread') != true) {
+            API.mark_read(item_id);
+        }
     }
 
     function scroll_to_item(index) {
@@ -144,6 +151,13 @@ var UI = (function($) {
             // this should probably be returned by the api
             var new_state = item_state == 'read' ? 'unread' : 'read';
             update_item_state(item_id, new_state);
+
+            // the user has manually kept this item unread
+            // keep track so that the item is not marked as read
+            // automatically when the item is next viewed
+            if (new_state == 'unread') {
+                item.data('kept-unread', true);
+            }
         });
 	};
 
